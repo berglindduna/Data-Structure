15a1c42e0c29d467bab85fb46e1af5b07fa4bc41
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-14 19:33:40 +0000

    Add files via upload

---
+++ ArrayList/arraylist.cpp
@@ -0,0 +1,140 @@
+// 21.01.2016
+
+#include "arraylist.h"
+#include <assert.h>
+
+template<class T>
+ArrayList<T>::ArrayList(int size)
+{
+    maxSize = size;
+    currSize = 0;
+    moveToStart();
+    arr = new T[maxSize];
+}
+
+template<class T>
+ArrayList<T>::~ArrayList()
+{
+    delete [] arr;
+}
+
+template<class T>
+void ArrayList<T>::append(T elem)
+{
+    if(currSize == maxSize)
+    {
+        maxSize = maxSize * 2;
+        T* newArr;
+        newArr = new T[maxSize];
+
+        for (int i = 0; i < currSize; i++)
+            newArr[i] = arr[i];
+
+        delete [] arr;
+        arr = newArr;
+    }
+
+    arr[currSize] = elem;
+    currSize++;
+}
+
+
+template<class T>
+void ArrayList<T>::insert(T elem)
+{
+     if(currSize == maxSize)
+    {
+        maxSize = maxSize * 2;
+        T* newArr;
+        newArr = new T[maxSize];
+
+        for (int i = 0; i < currSize; i++)
+            newArr[i] = arr[i];
+
+        delete [] arr;
+        arr = newArr;
+    }
+
+    currSize++;
+    T tmp = arr[currElemPos];
+
+    for (int i = currElemPos; i < (currSize - 1); i++)
+    {
+        T tmp2 = arr[i + 1];
+        arr[i + 1] = tmp;
+        tmp = tmp2;
+    }
+
+    arr[currElemPos] = elem;
+}
+
+template<class T>
+void ArrayList<T>::next()
+{
+    if (currElemPos < currSize)
+        currElemPos++;
+}
+
+template<class T>
+void ArrayList<T>::prev()
+{
+    if (currElemPos > 0)
+        currElemPos--;
+}
+
+template<class T>
+void ArrayList<T>::moveToStart()
+{
+    currElemPos = 0;
+}
+
+template<class T>
+void ArrayList<T>::moveToEnd()
+{
+    currElemPos = currSize;
+}
+
+template<class T>
+void ArrayList<T>::moveToPos( int pos )
+{
+    currElemPos = pos;
+}
+
+template<class T>
+void ArrayList<T>::remove()
+{
+    currSize -= 1;
+
+    for (int i=currElemPos; i< currSize; i++)
+        arr[i] = arr[i+1];
+}
+
+template<class T>
+void ArrayList<T>::clear()
+{
+    T* newArr;
+    newArr = new T[maxSize];
+    delete [] arr;
+    arr = newArr;
+    moveToStart();
+    currSize = 0;
+}
+
+template<class T>
+int ArrayList<T>::length() const
+{
+    return currSize;
+}
+
+template<class T>
+int ArrayList<T>::currPos() const
+{
+    return currElemPos;
+}
+
+template<class T>
+T ArrayList<T>::value() const
+{
+    assert(currElemPos >=0 &&  currElemPos < currSize); // No current element
+    return arr[currElemPos];
+}
---
+++ ArrayList/arraylist.h
@@ -0,0 +1,34 @@
+//21.01.16
+// Implement the template class ArrayList
+// The main program first reads in a single integer, initialSize, denoting what the initial size should be.  The next integer, count, denotes how many elements follow, and then count values are given which comprise the list elements.  The above is first carried out for a list of integers and then a list of strings.
+
+#ifndef ARRAYLIST_H
+#define ARRAYLIST_H
+
+template <class T>
+class ArrayList
+{
+    public:
+        ArrayList(int size);        // Constructor
+        ~ArrayList();               // Destructor
+        void append(T elem);     // Appends an element at the end of the list
+        void insert(T elem);        // Inserts an element to the current position
+        void next();                // Moves the current position one step right
+        void prev();                //Moves the current position one step left
+        void moveToStart();         // Set the current position to the start of the list
+        void moveToEnd();           // Set the current position to the end of the list
+        void moveToPos(int pos);  // Set the current position to pos
+        void remove();              // Removes the element in the current position of the list
+        void clear();               // Removes all the elements from the list
+        int currPos() const;        // Returns the position of the current element
+        int length() const;         // Returns the current length of the list
+        T value() const;          //Returns the current element
+
+    private:
+        int maxSize;        // Maximum size of list
+        int currSize;       // Current number of list items
+        int currElemPos;    // Position of the current element of the list
+        T* arr;           // A pointer to the array holding the list elements
+};
+
+#endif // ARRAYLIST_H
