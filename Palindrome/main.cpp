819c1f4f281aa28f63c0f8262757f779fb0569ef
Berglind D. Sigurdardottir <berglinds14@ru.is>
2016-10-23 17:21:10 +0000

    Add files via upload

---
+++ Palindrome/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include "palindrome.h"
+
+using namespace std;
+
+// Tests
+int main()
+{
+    char a[] = "abcdcba";
+    char b[] = "1221";
+    char c[] = "ablab";
+
+    if (palindrome(a, 0, strlen(a)-1))
+        cout << a << " is a palindrome" << endl;
+
+    if (palindrome(b, 0, strlen(b)-1))
+        cout << b << " is a palindrome" << endl;
+
+    if (palindrome(c, 0, strlen(c)-1))
+        cout << c << " is a palindrome" << endl;
+
+    return 0;
+}
---
+++ Palindrome/palindrome.cpp
@@ -0,0 +1,8 @@
+// 18.02.2016
+
+#include "palindrome.h"
+
+bool palindrome(const char a[], int first, int last)
+{
+   return (last-first) < 1 || (a[first] == a[last] && palindrome(a, first+1, last-1));
+}
